23df3ec84779934e49037922af27f6d62b3cba07
elf <filonenko0406@gmail.com>
2021-08-05 14:46:40 +0800

    style fix

--- media.php
+++ media.php
@@ -8,14 +8,14 @@
 	<section class="section">
 		<div class="container">
 			<div class="for-clients">
-				<div class="for-clients__list">
-					<div class="card card_light-blue for-clients-card for-clients__item">
+				<div class="for-clients__list card-list">
+					<div class="card card_light-blue for-clients-card for-clients__item card-list__item">
 						<div class="for-clients-card__title">Новости</div>
 						<div class="for-clients-card__text">Самые последние новости района и ЖК</div>
 						<div class="for-clients-card__btn"><a href="<?php echo site_url('news/') ?>" class="btn btn_yellow-border">посмотреть</a></div>
 						<div class="for-clients-card__icon icon icon_file-text"></div>
 					</div>
-					<div class="card card_light-blue for-clients-card for-clients__item">
+					<div class="card card_light-blue for-clients-card for-clients__item card-list__item">
 						<div class="for-clients-card__title">Live</div>
 						<div class="for-clients-card__text">Прямые трансляции</div>
 						<div class="for-clients-card__btn"><a href="<?php echo site_url('live/') ?>" class="btn btn_yellow-border">посмотреть</a></div>
--- real-estate.php
+++ real-estate.php
@@ -8,16 +8,16 @@
     <section class="section">
         <div class="container">
             <div class="real-estate">
-                <div class="real-estate__list">
-                    <div class="card card_light-blue real-estate-card real-estate__item">
+                <div class="real-estate__list card-list">
+                    <div class="card card_light-blue real-estate-card real-estate__item card-list__item">
                         <div class="real-estate-card__title">Новостройки</div>
                         <div class="real-estate-card__btn"><a href="<?php echo site_url('novostrojki/')?>" class="btn btn_yellow-border">посмотреть</a></div>
                     </div>
-                    <div class="card card_light-blue real-estate-card real-estate__item">
+                    <div class="card card_light-blue real-estate-card real-estate__item card-list__item">
                         <div class="real-estate-card__title">Коммерческая</div>
                         <div class="real-estate-card__btn"><a href="<?php echo site_url('kommercheskaya-nedvizhimost/')?>" class="btn btn_yellow-border">посмотреть</a></div>
                     </div>
-                    <div class="card card_light-blue real-estate-card real-estate__item">
+                    <div class="card card_light-blue real-estate-card real-estate__item card-list__item">
                         <div class="real-estate-card__title">Машиноместа</div>
                         <div class="real-estate-card__btn"><a href="<?php echo site_url('mashinomesta/')?>" class="btn btn_yellow-border">посмотреть</a></div>
                     </div>
